8177a008bca1266b64c5ee9d1f60dc14663cf757
Sergey Bigulov <biguloff@gmail.com>
2015-05-12 08:32:42 +0300

    Head file added

--- treestruc.c
+++ treestruc.c
@@ -36,4 +36,45 @@ postorder(root->right);
 if(root->info) printf("%c ", root->info);
 }
 
+
+struct tree *dtree(struct tree *root, char key)
+{
+	struct tree *p,*p2;
+	if(!root) return root; /* вершина не найдена */
+	if(root->info == key) { /* удаление корня */
+	/* это означает пустое дерево */
+	if(root->left == root->right){
+	free(root);
+	return NULL;
+}
+	/* или если одно из поддеревьев пустое */
+else if(root->left == NULL) 
+{
+	p = root->right;
+	free(root);
+	return p;
+}
+else if(root->right == NULL)
+{
+	p = root->left;
+	free(root);
+	return p;
+}
+	/* или есть оба поддерева */
+else 
+{
+	p2 = root->right;
+	p = root->right;
+	while(p->left) p = p->left;
+	p->left = root->left;
+	free(root);
+	return p2;
+}
+}
+if(root->info < key) root->right = dtree(root->right, key);
+else root->left = dtree(root->left, key);
+
+return root;
+}
+
 /* CS MSU 2015
---
+++ treestruc.h
@@ -0,0 +1,19 @@
+#ifdef _treelist.h_
+#define _treelist.h_
+#include <stdio.h>
+struct tree 
+{
+char info;
+struct tree *left;
+struct tree *right; 
+};
+
+void inorder(struct tree *root); /* симметричный обход */
+
+void preorder(struct tree *root); /* упорядоченный обход */
+
+void postorder(struct tree *root); /* реверсивный обход */
+
+struct tree *dtree(struct tree *root, char key); /* удаление вершины */
+
+#endif
