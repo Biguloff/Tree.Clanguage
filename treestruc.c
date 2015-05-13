683f50e96879822382137cdaea756cba07942133
Sergey Bigulov <biguloff@gmail.com>
2015-05-14 01:45:02 +0300

    Sourse code file

--- treestruc.c
+++ treestruc.c
@@ -2,38 +2,38 @@
 
 struct tree 
 {
-char info;
-struct tree *left;
-struct tree *right; 
+	char info;
+	struct tree *left;
+	struct tree *right; 
 };
 
 
 void inorder(struct tree *root)
 {
-if(!root) return;
+	if(!root) return;
 }
-inorder(root->left);
-if(root->info) printf("%c ", root->info);
-inorder(root->right);
+	inorder(root->left);
+	if(root->info) printf("%c ", root->info);
+	inorder(root->right);
 
 
 
 void preorder(struct tree *root)
 {
-if(!root) return;
-if(root->info) printf("%c ", root->info);
-preorder(root->left);
-preorder(root->right);
+	if(!root) return;
+	if(root->info) printf("%c ", root->info);
+	preorder(root->left);
+	preorder(root->right);
 }
 
 
 void postorder(struct tree *root)
 {
-if(!root) return;
+	if(!root) return;
 }
-postorder(root->left);
-postorder(root->right);
-if(root->info) printf("%c ", root->info);
+	postorder(root->left);
+	postorder(root->right);
+	if(root->info) printf("%c ", root->info);
 }
 
 
